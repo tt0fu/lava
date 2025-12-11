299346bcb2fff511893b38cfb94c532a2c1da7d6
ttofu <tt0fu@users.noreply.github.com>
2025-12-11 06:14:11 +0300

    very bad packaging

--- flake.nix
+++ flake.nix
@@ -20,53 +20,78 @@
       let
         overlays = [ (import rust-overlay) ];
         pkgs = import nixpkgs { inherit system overlays; };
+
+        packages = with pkgs; [
+          (rust-bin.stable.latest.minimal.override {
+            extensions = [
+              "clippy"
+              "rust-analyzer"
+              "rust-docs"
+              "rust-src"
+            ];
+          })
+          (rust-bin.selectLatestNightlyWith (toolchain: toolchain.rustfmt))
+
+          pkg-config
+          makeWrapper
+
+          alsa-lib
+          alsa-utils
+          pulseaudio
+          pipewire
+
+          shaderc
+          spirv-tools
+          vulkan-loader
+          vulkan-tools
+          vulkan-tools-lunarg
+          vulkan-validation-layers
+
+          libGL
+
+          libxkbcommon
+          wayland
+          wayland.dev
+          xorg.libX11
+          xorg.libXcursor
+          xorg.libXi
+          xorg.libXrandr
+        ];
       in
       {
         devShells.default =
           with pkgs;
-          mkShell rec {
-            buildInputs = [
-              (rust-bin.stable.latest.minimal.override {
-                extensions = [
-                  "clippy"
-                  "rust-analyzer"
-                  "rust-docs"
-                  "rust-src"
-                ];
-              })
-              (rust-bin.selectLatestNightlyWith (toolchain: toolchain.rustfmt))
+          mkShell {
+            buildInputs = packages;
 
-              alsa-lib
-              alsa-utils
-              pulseaudio
-              pipewire
-
-              shaderc
-              spirv-tools
-              vulkan-loader
-              vulkan-tools
-              vulkan-tools-lunarg
-              vulkan-validation-layers
-
-              libGL
-
-              libxkbcommon
-              wayland
-              xorg.libX11
-              xorg.libXcursor
-              xorg.libXi
-              xorg.libXrandr
-
-              bashInteractive
-            ];
-
-            LD_LIBRARY_PATH = lib.makeLibraryPath buildInputs;
+            LD_LIBRARY_PATH = lib.makeLibraryPath packages;
             PKG_CONFIG_PATH = "${alsa-lib.dev}/lib/pkgconfig";
             SHADERC_LIB_DIR = lib.makeLibraryPath [ shaderc ];
             VK_LAYER_PATH = "${vulkan-validation-layers}/share/vulkan/explicit_layer.d";
 
-            shellHook = '' export SHELL="${pkgs.bashInteractive}/bin/bash"; '';
+            shellHook = ''export SHELL="${pkgs.bashInteractive}/bin/bash"; '';
           };
+        defaultPackage = pkgs.rustPlatform.buildRustPackage {
+          pname = "lava";
+          version = "0.0.1";
+          doCheck = false;
+          cargoLock = {
+            lockFile = ./Cargo.lock;
+          };
+          src = ./.;
+          buildInputs = packages;
+          nativeBuildInputs = packages;
+          env = with pkgs; {
+            LD_LIBRARY_PATH = lib.makeLibraryPath packages;
+            PKG_CONFIG_PATH = "${alsa-lib.dev}/lib/pkgconfig";
+            SHADERC_LIB_DIR = lib.makeLibraryPath [ shaderc ];
+            VK_LAYER_PATH = "${vulkan-validation-layers}/share/vulkan/explicit_layer.d";
+          };
+          postFixup = ''
+            wrapProgram $out/bin/lava \
+              --set LD_LIBRARY_PATH ${pkgs.lib.makeLibraryPath packages}
+          '';
+        };
       }
     );
 }
