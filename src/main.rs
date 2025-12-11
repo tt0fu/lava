2a7bd10d25b906bdb7f2049017c2196437f8d3e0
ttofu <tt0fu@users.noreply.github.com>
2025-12-11 18:32:04 +0300

    remove duplicated model data

--- src/main.rs
+++ src/main.rs
@@ -6,44 +6,6 @@ use std::error::Error;
 use vulkano::{buffer::BufferContents, pipeline::graphics::vertex_input::Vertex};
 use winit::event_loop::EventLoop;
 
-#[derive(BufferContents, Vertex)]
-#[repr(C)]
-pub struct Position {
-    #[format(R32G32_SFLOAT)]
-    position: [f32; 2],
-}
-
-pub const POSITIONS: [Position; 4] = [
-    Position {
-        position: [-1.0, -1.0],
-    },
-    Position {
-        position: [-1.0, 1.0],
-    },
-    Position {
-        position: [1.0, -1.0],
-    },
-    Position {
-        position: [1.0, 1.0],
-    },
-];
-
-#[derive(BufferContents, Vertex)]
-#[repr(C)]
-pub struct Uv {
-    #[format(R32G32_SFLOAT)]
-    uv: [f32; 2],
-}
-
-pub const UVS: [Uv; 4] = [
-    Uv { uv: [0.0, 0.0] },
-    Uv { uv: [0.0, 1.0] },
-    Uv { uv: [1.0, 0.0] },
-    Uv { uv: [1.0, 1.0] },
-];
-
-pub const INDICES: [u16; 6] = [0, 1, 2, 1, 2, 3];
-
 // fn get_braile(vecs: Vec<u8>) -> char {
 //     let to_add: u32 = vecs.iter().map(|a| 1 << a).sum();
 //     char::from_u32(('⠀' as u32) + to_add).unwrap()
